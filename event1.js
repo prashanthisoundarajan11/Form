3b5967dd3380e80784e53b51df82b322f39424ab
Prashanthi Soundrajan <79076790+prashanthisoundarajan11@users.noreply.github.com>
2023-04-12 07:30:40 +0200

    Add files via upload

---
+++ event1.js
@@ -0,0 +1,8 @@
+function foo(){
+var fname = document.getElementById("fname").value
+console.log(fname)
+var lname = document.getElementById("lname").value
+console.log(lname)
+var mail = document.getElementById("mail").value
+console.log(mail)
+}
